ab38f911aae651ec7fba88723d800efc6c485f60
arantebillywilson <arantebillywilson@gmail.com>
2017-07-02 10:16:46 -0400

    banner.rb: Initial commit

---
+++ firehose/banner.rb
@@ -0,0 +1,9 @@
+# banner.rb
+#
+# Written by Billy Wilson Arante <arantebillywilson@gmail.com>
+# Last updated on 2017/07/02 PHT
+
+puts "################################"
+puts "# Coded by Billy Wilson Arante #"
+puts "# I am a coding machine        #"
+puts "################################"
