40153be8fee5c39ddf448609f8e1e195c591c13a
arantebillywilson <arantebillywilson@gmail.com>
2017-07-02 10:40:40 -0400

    input.rb: Added file descriptions

--- firehose/input.rb
+++ firehose/input.rb
@@ -1,3 +1,8 @@
+# input.rb
+#
+# Written by Billy Wilson Arante <arantebillywilson@gmail.com>
+# Last updated on 2017/07/02 PHT
+
 text = "Today, I wrote code"
 text = "Tomorrow I will code, too"
 puts text
