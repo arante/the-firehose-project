fc590f01b5c13d513c059a4a8d8ee186570e1c0d
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-08-23 00:06:15 +0800

    mabuhay.rb: Coding exercise

---
+++ firehose/mabuhay.rb
@@ -0,0 +1,8 @@
+puts "What is your name?"
+user_name = gets.chomp
+
+puts "Where do you leave?"
+user_location = gets.chomp
+
+welcome_msg = "Hello, #{user_name} of #{user_location}! Welcome to Ruby programming!"
+puts welcome_msg
