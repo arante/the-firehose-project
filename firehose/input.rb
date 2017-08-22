3eb03093af5a4983e6bdbfc64e35f39fdeeb47d8
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-08-22 23:39:00 +0800

    input.rb: Getting User Input

--- firehose/input.rb
+++ firehose/input.rb
@@ -3,6 +3,5 @@
 # Written by Billy Wilson Arante <arantebillywilson@gmail.com>
 # Last updated on 2017/07/02 PHT
 
-text = "Today, I wrote code"
-text = "Tomorrow I will code, too"
-puts text
+puts "Enter your name:"
+name = gets
