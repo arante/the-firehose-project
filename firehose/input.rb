2d973dc8ac78ae591012de2d4afa4818146fc3bd
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-08-22 23:58:57 +0800

    input.rb: Templating with multiple variables

--- firehose/input.rb
+++ firehose/input.rb
@@ -6,5 +6,8 @@
 puts "Enter your name:"
 user_name = gets.chomp
 
-greeting = "Hello, #{user_name}. You are awesome!"
+puts "Enter something you own:"
+user_thing = gets.chomp
+
+greeting = "Hello, #{user_name}. What an excellent #{user_thing} you have!"
 puts greeting
